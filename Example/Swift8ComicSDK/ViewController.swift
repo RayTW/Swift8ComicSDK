62c2a15daeb555500eca918d770068be4222ece0
Ray Lee <ray00000sina@gmail.com>
2017-06-21 15:39:29 +0800

    add : 	加入測試解析全部漫畫列表test function。

--- Example/Swift8ComicSDK/ViewController.swift
+++ Example/Swift8ComicSDK/ViewController.swift
@@ -33,6 +33,15 @@ class ViewController: UIViewController {
 
 
     }
+    @IBAction func getAllTest(_ sender: Any) {
+        let html = "<tr><td style=\"width:20%;\"><a href=\"/html/12461.html\" onmouseover='showthumb(12461,this);' onmouseout='hidethumb();'>豬排咖喱飯的日子</a></td><td style=\"width:20%;\"><a href=\"/html/14489.html\" onmouseover='showthumb(14489,this);' onmouseout='hidethumb();'>豬人居然是鎮主夫什麼的</a></td><td style=\"width:20%;\"><a href=\"/html/4227.html\" onmouseover='showthumb(4227,this);' onmouseout='hidethumb();'>竹刀少女</a></td><td style=\"width:20%;\"><a href=\"/html/9889.html\" onmouseover='showthumb(9889,this);' onmouseout='hidethumb();'>竹刀少女C</a></td><td style=\"width:20%;\"><a href=\"/html/6533.html\" onmouseover='showthumb(6533,this);' onmouseout='hidethumb();'>竹劍少年</a></td></tr><tr><td style=\"width:20%;\"><a href=\"/html/13115.html\" onmouseover='showthumb(13115,this);' onmouseout='hidethumb();'>竹林之花，不眠之姬</a></td><td style=\"width:20%;\"><a href=\"/html/13304.html\" onmouseover='showthumb(13304,this);' onmouseout='hidethumb();'>竹馬未完成</a></td><td style=\"width:20%;\"><a href=\"/html/12128.html\" onmouseover='showthumb(12128,this);' onmouseout='hidethumb();'>主從百合本</a></td><td style=\"width:20%;\"><a href=\"/html/5748.html\" onmouseover='showthumb(5748,this);' onmouseout='hidethumb();'>主將!地院家若美</a></td><td style=\"width:20%;\"><a href=\"/html/3713.html\" onmouseover='showthumb(3713,this);' onmouseout='hidethumb();'>主教D</a></td></tr><tr>"
+        
+        let comics : [Comic] = R8Comic.get().getParser().allComics(htmlString: html)
+        
+        for comic : Comic  in comics {
+            print("id==>\(comic.getId()), name[\(comic.getName())]")
+        }
+    }
    
 }
 
